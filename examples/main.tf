7f39293b0c4a7e48efaabb1d873000f86c5d5b7c
Xingchi Jin <xingchi.jin@harness.io>
2023-01-16 18:26:53 -0800

    example main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,14 +1,14 @@
 module "delegate" {
-  source = "harness/kubernetes-delegate/harness"
+  source = "harness/harness-delegate/kubernetes"
   version = "0.1.5"
 
-  account_id = ""
-  delegate_token = ""
-  delegate_name = "example"
+  account_id = "PUT_YOUR_ACCOUNT_ID"
+  delegate_token = "PUT_YOUR_DELEGATE_TOKEN"
+  delegate_name = "PUT_YOUR_DELEGATE_NAME"
   namespace = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io/gratis"
-  # delegate_image = "harness/delegate:22.12.77802"
-  replicas = 1
+  manager_endpoint = "PUT_YOUR_MANAGER_URL"
+  # delegate_image = "harness/delegate:22.11.77802"
+  replica = 1
   upgrader_enabled = false
 
   # Additional optional values to pass to the helm chart
