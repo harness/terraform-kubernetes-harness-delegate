4f0e8c0d1717eeca774cfcdf3d4950edeb31356b
xingchi.jin <xingchi.jin@harness.io>
2023-08-14 19:35:47 -0700

    bump helm provider version to 2.9

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.5.1"
+      version = "2.9.0"
     }
     utils = {
       source  = "cloudposse/utils"
